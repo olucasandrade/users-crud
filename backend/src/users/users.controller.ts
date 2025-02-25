e7e7e934d4ea063c81c4dff9a8c1f1689f695419
Lucas Andrade <lafdesouza2002@outlook.com>
2025-02-24 22:28:36 -0300

    feat: Create user controller

---
+++ backend/src/users/users.controller.ts
@@ -0,0 +1,52 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User as UserModel } from '@prisma/client';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() createUserDto: CreateUserDto): Promise<UserModel> {
+    return this.usersService.create(createUserDto);
+  }
+
+  @Get()
+  findAll(): Promise<UserModel[]> {
+    return this.usersService.findAll({});
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string): Promise<UserModel | null> {
+    return this.usersService.find({ id: Number(id) });
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
+    return this.usersService.update({
+      where: { id: Number(id) },
+      data: updateUserDto,
+    });
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<{ deleted: boolean }> {
+    try {
+      await this.usersService.delete({ id: Number(id) });
+      return { deleted: true };
+    } catch (e) {
+      console.error(e);
+      return { deleted: false };
+    }
+  }
+}
