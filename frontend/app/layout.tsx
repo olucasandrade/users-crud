09ab95150567c9bdef28fd03e3d218d09630aa2a
Lucas Andrade <lafdesouza2002@outlook.com>
2025-02-24 23:30:50 -0300

    feat: add providers

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import { StackProvider, StackTheme } from "@stackframe/stack";
+import { stackServerApp } from "../stack";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryProvider } from "./queryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,17 +25,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = new QueryClient()
-
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </body>
+      ><StackProvider app={stackServerApp}><StackTheme>
+        <QueryProvider>
+         {children}
+        </QueryProvider>
+      </StackTheme></StackProvider></body>
     </html>
   );
 }
